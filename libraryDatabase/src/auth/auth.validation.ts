57d0ac5f317d8906e53fd95e3243e4af04468e4f
Gathoni-K <gathonikarume@gmail.com>
2026-04-02 12:16:06 +0300

    finished auth validation with Zod

---
+++ libraryDatabase/src/auth/auth.validation.ts
@@ -0,0 +1,79 @@
+import { z } from 'zod';
+
+// Reusable fields
+
+const emailSchema = z
+    .string({ error: 'Email is required'})
+    .email('Invalid email format')
+    .toLowerCase()
+    .trim();
+
+const passwordSchema = z
+    .string({ error: 'Password is required'})
+    .min(8, 'Password must be at least 8 characters')
+    .max(128, 'Password is too long');
+
+const usernameSchema = z
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(32, 'Username is too long')
+    .regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain numbers, letters or underscores');
+
+// Auth Schemas.
+export const registerSchema = z.object({
+    email: emailSchema,
+    password: passwordSchema,
+    username: usernameSchema,
+    confirmPassword: z.string(),
+});
+
+export const loginSchema = z.object({
+    email: emailSchema,
+    password: passwordSchema,
+    username: usernameSchema,
+    confirmPassword: z.string(),
+});
+
+export const forgotPasswordSchema = z.object({
+    email: emailSchema,
+});
+
+export const resetPasswordSchema = z.object({
+    token: z.string({ error: 'Token is required'}),
+    password: passwordSchema,
+    confirmPassword: z.string(),
+})
+.refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmNewPassword'],
+});
+
+export const changePasswordSchema = z.object({
+    currrentPassword: z.string({ error: 'Password is required'}),
+    newPassword: passwordSchema,
+    confirmNewPassword: z.string(),
+})
+.refine((data) => data.newPassword === data.confirmNewPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmNewPassword'],
+})
+.refine((data) => data.currrentPassword !== data.newPassword, {
+    message: 'New password must differ from the previous one',
+    path: ['newPassword']
+    //path tells Zod which field to attach the error to.
+});
+
+
+// Token/Session Schema
+
+export const refreshTokenSchema = z.object({
+    refreshToken: z.string({ error: 'Refresh token is required'}),
+});
+
+// inferred types
+
+export type LoginInput = z.infer<typeof loginSchema>;
+export type RegisterInput = z.infer<typeof registerSchema>;
+export type ResetPasswordInput = z.infer<typeof resetPasswordSchema>;
+export type ForgotPasswordInput = z.infer<typeof forgotPasswordSchema>;
+export type refreshTokenInput = z.infer<typeof refreshTokenSchema>;
